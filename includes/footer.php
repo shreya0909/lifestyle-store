bd02601b5d8d92f0e387732176e1b77da1a786a6
Shreya <shreyaag27@gmail.com>
2021-07-28 18:22:06 +0530

    commit

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer>
+    <div class="container">
+        <center>
+            <p>Copyright &copy; Lifestyle Store. All rights reserved | contact us: ...</p>
+        </center>
+    </div>
+</footer>
